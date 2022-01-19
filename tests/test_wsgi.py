b7a77fb32fd4a93ad73323e94e6ec5fedefed22e
Herilaza Razafindramanitra <herilaza.razafindramanitra@gmail.com>
2022-01-19 15:11:24 +0300

    Updating api/v1/produits

--- tests/test_wsgi.py
+++ tests/test_wsgi.py
@@ -12,3 +12,9 @@ class ApplicationTest(TestCase):
         body = response.data.decode()
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Hello" in body)
+
+    def test_read_many_products(self):
+        response = self.client.get('/api/v1/produits')
+        products = response.json
+        self.assertIsInstance(products, dict)
+        self.assertGreater(len(products), 2)
--- wsgi.py
+++ wsgi.py
@@ -3,14 +3,12 @@ from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello world"
-
-@app.route('/api/v1/produits')
-def list_products():
-    PRODUCTS = {
+PRODUCTS = {
     1: { 'id': 1, 'name': 'Skello' },
     2: { 'id': 2, 'name': 'Socialive.tv' },
+    3: { 'id': 3, 'name': 'Le Wagon'},
     }
-    return jsonify(PRODUCTS)
+
+@app.route('/api/v1/produits')
+def read_many_products():    
+    return jsonify(PRODUCTS), 200
